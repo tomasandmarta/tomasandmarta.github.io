b16b3cda635089130933ffb75185fabe6c467f0b
L-Marta <116031554+L-Marta@users.noreply.github.com>
2023-07-18 00:30:51 +0200

    Mostylallfornow17/18072023

---
+++ main_pl.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+    $('.accordion-content').hide();
+    $('#accordion-button-1').on('click', () => {
+        $('#accordion-content-1').toggle();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-4').hide();
+        $('#accordion-content-6').hide();
+        $('#accordion-content-5').hide();
+        $('#up1').toggleClass('rotate');
+        $('#up5').removeClass('rotate');
+        $('#up6').removeClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up4').removeClass('rotate');
+    })
+    $('#accordion-button-2').on('click', () => {
+        $('#accordion-content-2').toggle();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-5').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-4').hide();
+        $('#accordion-content-6').hide();
+        $('#up2').toggleClass('rotate');
+        $('#up5').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up4').removeClass('rotate');
+        $('#up6').removeClass('rotate');
+    })
+    $('#accordion-button-3').on('click', () => {
+        $('#accordion-content-3').toggle();
+        $('#accordion-content-5').hide();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-4').hide();
+        $('#accordion-content-6').hide();
+        $('#up3').toggleClass('rotate');
+        $('#up5').removeClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up4').removeClass('rotate');
+        $('#up6').removeClass('rotate');
+    })
+    $('#accordion-button-4').on('click', () => {
+        $('#accordion-content-4').toggle();
+        $('#accordion-content-5').hide();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-6').hide();
+        $('#up4').toggleClass('rotate');
+        $('#up5').removeClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up6').removeClass('rotate');
+    })
+    $('#accordion-button-5').on('click', () => {
+        $('#accordion-content-5').toggle();
+        $('#accordion-content-4').hide();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-6').hide();
+        $('#up5').toggleClass('rotate');
+        $('#up4').toggleClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up6').removeClass('rotate');
+    })
+    $('#accordion-button-6').on('click', () => {
+        $('#accordion-content-6').toggle();
+        $('#accordion-content-4').hide();
+        $('#accordion-content-2').hide();
+        $('#accordion-content-3').hide();
+        $('#accordion-content-1').hide();
+        $('#accordion-content-5').hide();
+        $('#up6').toggleClass('rotate');
+        $('#up4').toggleClass('rotate');
+        $('#up2').removeClass('rotate');
+        $('#up3').removeClass('rotate');
+        $('#up1').removeClass('rotate');
+        $('#up5').removeClass('rotate');
+    })
+})
